c5ee921e5a09857adeb8ad590791614511b4b281
Dnyaneshvn <dvnimbalkar24@gmail.com>
2025-01-06 14:50:48 +0000

    updated configs

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['lh3.googleusercontent.com'],
+        domains: ['lh3.googleusercontent.com', "avatars.githubusercontent.com"],
+        typescript: {
+          ignoreBuildErrors: true,
+        },
+        eslint: {
+          ignoreDuringBuilds: true,
+        },
+        output: "standalone",
       },
     };
     
