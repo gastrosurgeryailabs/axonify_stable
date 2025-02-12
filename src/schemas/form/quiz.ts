4719c502ddd3e9f3150974b403a1c49d20a54f10
Dnyaneshvn <dvnimbalkar24@gmail.com>
2025-02-12 09:00:10 +0530

    prod changes

--- src/schemas/form/quiz.ts
+++ src/schemas/form/quiz.ts
@@ -70,7 +70,7 @@ export const socialMediaSchema = z.object({
 });
 
 export const quizCreationSchema = z.object({
-    topic: z.string().min(4, {message:"Topic must be atleast 4 characters long"}).max(500),
+    topic: z.string().min(2, {message:"Topic must be atleast 2 characters long"}).max(500),
     type: z.enum(['mcq', 'open_ended']),
     amount: z.number().min(1).max(10),
     targetLanguage: z.string().default('en'),
